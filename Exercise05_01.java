ed320652a21ba742a0f69c5c27a30d44a61a2b7e
Kittipong Mawan <do-monn@hotmail.com>
2012-11-11 18:28:39 +0700

    aaa
    gggg

---
+++ Exercise05_01.java
@@ -0,0 +1,11 @@
+public class Exercise05_01
+{
+    public static void main(String[] args)
+    {
+        double  degree = Math.toRadians(90),
+                result = Math.sin(degree);
+
+        System.out.println(result);
+    }
+}
+
